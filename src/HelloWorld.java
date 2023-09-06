e9fa990dd294715d1b7bb29e37523d01b91803e9
romanceprofessor <104313221+romanceprofessor@users.noreply.github.com>
2023-09-06 13:26:39 +0900

    출력하기

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("안녕자바!");
+		System.out.println("안녕 java!");
 	
 	}
 
